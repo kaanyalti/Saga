f64a78f48ce8b7044786853d447403ffedaea68a
Corrina Chow <corrina.yy.chow@gmail.com>
2018-05-31 23:30:22 +0000

    OAuth2 on front-end

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,12 @@
 class ApplicationRecord < ActiveRecord::Base
+  protect_from_forgery with: :exception
+  helper_method :current_user
+
   self.abstract_class = true
+
+  private
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
 end
