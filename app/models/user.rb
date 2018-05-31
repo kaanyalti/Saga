bc5084506bb1b25933ac206785515ed42797e243
Corrina Chow <corrina.yy.chow@gmail.com>
2018-05-31 21:18:01 +0000

    Add OmniAuth method to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,14 @@
 class User < ApplicationRecord
-  # has_many :user_videos
   has_many :videos
-  # , through: :user_videos
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_initialize.tap do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
+      user.name = auth.info.name
+      user.oauth_token = auth.credentials.token
+      user.oauth_expires_at = Time.at(auth.credentials.expires_at)
+      user.save!
+    end
+  end
 end
