6e10278fd77f96519aac13267e59904b8ec36f37
Corrina Chow <corrina.yy.chow@gmail.com>
2018-06-01 00:21:05 +0000

    Fix CORS issue

--- config/application.rb
+++ config/application.rb
@@ -15,7 +15,9 @@ require "action_cable/engine"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
+config.action_dispatch.default_headers = {
+    'Access-Control-Allow-Origin' => 'http://localhost:3000/',
+  }
 module FinalProject
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
