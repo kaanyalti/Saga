3c33d2b0a347c6bb3abec055964652ecd20554b3
steveabouem <steveabouem@gmail.com>
2018-06-01 23:10:52 +0000

    n/A

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,8 @@
 class HomeController < ApplicationController
   def show
+    db =
   end
 end
+# create var here if this is the endpoont youre doing jur ajax on. try to aggregate data on the backed instead
+# of doing the whole wokr on th front end
+# map rduce will need to b used at some point
