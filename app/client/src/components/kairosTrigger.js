ae9429fb259469f99e5400213f9e5a08a4b479d4
kaanyalti <ozankaanyalti@gmail.com>
2018-06-02 19:24:40 +0000

    issues retrieving video url

--- app/client/src/components/kairosTrigger.js
+++ app/client/src/components/kairosTrigger.js
@@ -4,12 +4,16 @@ import axios from "axios";
 class KairosTrigger extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      video_token: "b22a0044b9398de3dad66bd73c0f7869"
+    }
     this.callKairos = this.callKairos.bind(this);
   }
-  
+
   callKairos() {
+    const source_url = window.ZiggeoApi.Videos.source(this.state.video_token)
     axios
-      .get("/api/kairos")
+      .get(`/api/kairos?source=${source_url}`)
       .then(response => console.log(response))
       .catch(err => console.log("error: ", err));
   }
