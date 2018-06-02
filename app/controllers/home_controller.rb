843efd0872c2721f5c2b95498b202660c2cfbf9f
steveabouem <steveabouem@gmail.com>
2018-06-02 00:03:09 +0000

    All ready for ajax call

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,8 +7,3 @@ class HomeController < ApplicationController
 
   
 end
-# create var here if this is the endpoont youre doing jur ajax on. try to aggregate data on the backed instead
-# of doing the whole wokr on th front end
-# map rduce will need to b used at some point
-# namespaced controller in rails
-# create controller reaction, have it in the namespace, so that you access rpt/videos/reactions
