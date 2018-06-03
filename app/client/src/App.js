e568fa2c7ad94fa3f39c158bc9b62b297c07fed1
Corrina Chow <corrina.yy.chow@gmail.com>
2018-06-03 18:51:02 +0000

    Remove render videos component

--- app/client/src/App.js
+++ app/client/src/App.js
@@ -4,7 +4,6 @@ import "./App.css";
 import KairosTrigger from "./components/kairosTrigger";
 import GoogleSignIn from "./components/googleSignIn";
 import GoogleSignOut from "./components/googleSignOut";
-import renderVideos from "./components/renderVideos";
 
 class App extends Component {
   chartStyle = {
