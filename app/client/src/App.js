019293b6719d7af18ac502b85f3979f9e3eccc66
Corrina Chow <corrina.yy.chow@gmail.com>
2018-06-03 22:30:43 +0000

    Add working routes

--- app/client/src/App.js
+++ app/client/src/App.js
@@ -6,7 +6,28 @@ import ZiggeoRecorder from "./components/ziggeoRecorder";
 import GoogleSignIn from "./components/googleSignIn";
 import GoogleSignOut from "./components/googleSignOut";
 import EmotionsChart from "./components/analytics";
-import { Router, Route, Link, IndexRoute, hashHistory, browserHistory } from 'react-router'
+import { Link, Route, Switch } from 'react-router-dom';
+
+/* Home component */
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+)
+
+/* Category component */
+const Category = () => (
+  <div>
+    <h2>Category</h2>
+  </div>
+)
+
+/* Products component */
+const Products = () => (
+  <div>
+    <h2>Products</h2>
+  </div>
+)
 
 class App extends Component {
   getCoord() {
@@ -17,8 +38,33 @@ class App extends Component {
   }
   render() {
     return (
-      <div className="App">
-        <div id="chartContainer" style={this.chartStyle} />
+      <div>
+        <nav className="navbar navbar-light">
+          <ul className="nav navbar-nav">
+
+           /* Link components are used for linking to other views */
+            <li><Link to="/">Homes</Link></li>
+            <li><Link to="/category">Category</Link></li>
+            <li><Link to="/products">Products</Link></li>
+
+          </ul>
+         </nav>
+
+          /* Route components are rendered if the path prop matches the current URL */
+           <Route path="/" component={Home}/>
+           <Route path="/category" component={Category}/>
+           <Route path="/products" component={Products}/>
+
+      </div>
+    );
+  }
+}
+
+export default App;
+
+/*
+
+  <div id="chartContainer" style={this.chartStyle} />
         <GoogleSignIn />
         <GoogleSignOut />
         <KairosTrigger />
@@ -28,9 +74,5 @@ class App extends Component {
           y={this.getCoord().y}
         />
         <ZiggeoRecorder />
-      </div>
-    );
-  }
-}
 
-export default App;
+*/
