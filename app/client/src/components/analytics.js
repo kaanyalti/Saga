11525ab72fa60dbfadb59ae80d8cd1d7abbfefcb
Corrina Chow <corrina.yy.chow@gmail.com>
2018-06-04 22:16:18 +0000

    Add test files

--- app/client/src/components/analytics.js
+++ app/client/src/components/analytics.js
@@ -1,31 +1,31 @@
-import React, { Component } from "react";
-import axios from "axios";
-class EmotionsChart extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      title: props.title,
-      points: { x: this.props.x, y: this.props.y }
-    };
-  }
+// import React, { Component } from "react";
+// import axios from "axios";
+// class EmotionsChart extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       title: props.title,
+//       points: { x: this.props.x, y: this.props.y }
+//     };
+//   }
 
-  componentDidMount() {
-    let title = this.state.title;
-    let chart = new window.CanvasJS.Chart("chartContainer", {
-      title: { text: title },
-      data: [
-        {
-          type: "stackedArea",
-          dataPoints: []
-        }
-      ]
-    });
+//   componentDidMount() {
+//     let title = this.state.title;
+//     let chart = new window.CanvasJS.Chart("chartContainer", {
+//       title: { text: title },
+//       data: [
+//         {
+//           type: "stackedArea",
+//           dataPoints: []
+//         }
+//       ]
+//     });
 
-    chart.render();
-  }
-  render() {
-    return <div id="chartContainer" />;
-  }
-}
+//     chart.render();
+//   }
+//   render() {
+//     return <div id="chartContainer" />;
+//   }
+// }
 
-export default EmotionsChart;
+// export default EmotionsChart;
