e62ae4e5cd73704ca2de214c0ea38bc81d2cb00e
Corrina Chow <corrina.yy.chow@gmail.com>
2018-06-04 04:25:32 +0000

    Add Nav routes

--- app/client/src/App.js
+++ app/client/src/App.js
@@ -5,54 +5,51 @@ import ZiggeoRecorder from "./components/ziggeoRecorder";
 import GoogleSignIn from "./components/googleSignIn";
 import GoogleSignOut from "./components/googleSignOut";
 import EmotionsChart from "./components/analytics";
-import { Link, Route, Switch } from 'react-router-dom';
+import { Link, Route, Switch } from "react-router-dom";
+import Navbar from "./components/NavBar.jsx";
+import Video from "./CreatorDashboard/VideoOverview/Video.jsx";
+import VideoIndex from "./CreatorDashboard/VideoOverview/VideoIndex.jsx";
 
 /* Home component */
 const Home = () => (
   <div>
     <h2>Home</h2>
   </div>
-)
+);
 
 /* Login component */
 const Login = () => (
   <div>
     <h2>Login</h2>
   </div>
-)
-import Navbar from "./components/NavBar.jsx";
+);
 
-// class App extends Component {
-// import Component from './Dashboard/nameOfComponent.jsx'
-//   render() {
-//     return (
-//       <Component />
-//     )
-//   }
-// }
+/* Logout component */
+const Logout = () => (
+  <div>
+    <h2>Logout</h2>
+  </div>
+);
+
+/* Dashboard component */
+// const Dashboard = () => (
+//   <div>
+//     <h2>Dashboard</h2>
+//   </div>
+// );
+
 class App extends Component {
   render() {
     return (
       <div>
-    {/* HOME NAVBAR */}
-        <nav className="navbar navbar-light">
-          <ul className="nav navbar-nav">
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/login">Login</Link></li>
-          </ul>
-         </nav>
+        {/* DASHBOARD NAVBAR */}
+        <Navbar />
 
-    {/* DASHBOARD NAVBAR */}
 
-        <nav className="navbar navbar-light">
-          <ul className="nav navbar-nav">
-            <li><Link to="/">Dashboard</Link></li>
-            <li><Link to="/logout">Logout</Link></li>
-          </ul>
-         </nav>
-
-           <Route exact path="/" component={Home}/>
-           <Route path="/login" component={Login}/>
+        <Route exact path="/" component={Home} />
+        <Route path="/login" component={Login} />
+        <Route path="/logout" component={Logout} />
+        <Route path="/admin" component={VideoIndex} />
       </div>
     );
   }
