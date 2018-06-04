8ea4a0e4bbb52d42a494c467cb400f690127725a
Corrina Chow <corrina.yy.chow@gmail.com>
2018-06-04 04:24:06 +0000

    Render all videos from API call

---
+++ app/client/src/CreatorDashboard/VideoOverview/Video.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import videoIDs from '../../fakeYouTubeRes.js'
+import { Link } from 'react-router-dom'
+
+const Video = (props) => {
+  const video = videoIDs.get(props.match.params.video_id)
+  // if (!player) {
+  //   return <div>Sorry, but the player was not found</div>
+  // }
+  return (
+    <div>
+      <h1>Individual video page</h1>
+      <h2>{video.title}</h2>
+      <Link to='/videos'>Back</Link>
+    </div>
+  )
+}
+
+/*
+
+<h1>{player.name} (#{player.number})</h1>
+      <h2>Position: {player.position}</h2>
+
+*/
