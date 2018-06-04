96824fd9e504666535576830b5dc119166ec45b3
Corrina Chow <corrina.yy.chow@gmail.com>
2018-06-04 21:46:01 +0000

    Refactor

--- app/client/src/components/Layout/Navbar.jsx
+++ app/client/src/components/Layout/Navbar.jsx
@@ -1,28 +1,34 @@
 import React, { Component } from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 class Navbar extends Component {
   navStyle = {
     position: "fixed",
-    width: "100%",
-  }
+    width: "100%"
+  };
 
-  spanStyle= {
+  spanStyle = {
     float: "left",
-    color: 'rgba(193, 193, 193, 0.8)',
-    fontFamily: 'Montserrat',
+    color: "rgba(193, 193, 193, 0.8)",
+    fontFamily: "Montserrat",
     fontWeight: "bolder",
     fontSize: "2em"
-  }
+  };
   render() {
     return (
       <nav className="navbar navbar-light">
         <ul className="nav navbar-nav">
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/login">Login</Link></li>
-          <li><Link to="/admin">Dashboard</Link></li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/admin">Dashboard</Link>
+          </li>
         </ul>
-       </nav>
+      </nav>
     );
   }
 }
--- app/client/src/components/analytics.js
+++ app/client/src/components/analytics.js
@@ -1,31 +1,31 @@
-import React, { Component } from 'react'
-import axios from 'axios'
+import React, { Component } from "react";
+import axios from "axios";
 class EmotionsChart extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       title: props.title,
-      points: { x: this.props.x, y: this.props.y },
-    }
+      points: { x: this.props.x, y: this.props.y }
+    };
   }
 
   componentDidMount() {
-    let title = this.state.title
-    let chart = new window.CanvasJS.Chart('chartContainer', {
+    let title = this.state.title;
+    let chart = new window.CanvasJS.Chart("chartContainer", {
       title: { text: title },
       data: [
         {
-          type: 'stackedArea',
-          dataPoints: [],
-        },
-      ],
-    })
+          type: "stackedArea",
+          dataPoints: []
+        }
+      ]
+    });
 
-    chart.render()
+    chart.render();
   }
   render() {
-    return <div id="chartContainer" />
+    return <div id="chartContainer" />;
   }
 }
 
-export default EmotionsChart
+export default EmotionsChart;
