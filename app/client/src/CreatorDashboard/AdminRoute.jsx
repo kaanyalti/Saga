dd957feee1a63ff1d66193def3cefdfe7b6e7104
Corrina Chow <corrina.yy.chow@gmail.com>
2018-06-06 16:40:23 +0000

    Refactor class to arrow function

--- app/client/src/CreatorDashboard/AdminRoute.jsx
+++ app/client/src/CreatorDashboard/AdminRoute.jsx
@@ -7,36 +7,27 @@ import VideoList from "./VideoOverview/VideoList";
 // The VideoList component matches one of two different routes
 // depending on the full pathname
 
-const VideosAll = () => <h1>VideosAll</h1>;
-
-class AdminRoute extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    console.log(this.props);
-    return (
-      <Switch>
-        <Route
-          exact
-          path="/admin"
-          render={() =>
-            this.props.loggedIn ? (
-              <VideoList videoData={this.props.videoData} />
-            ) : (
-              <Redirect to="/login" />
-            )
-          }
-        />
-        <Route exact path="/admin/videos" component={VideosAll} />
-        <Route
-          path="/admin/videos/:video_id"
-          render={() => <VideoDetail videoData={this.props.videoData} />}
-        />
-      </Switch>
-    );
-  }
-}
+const AdminRoute = props => {
+  return (
+    <Switch>
+      <Route
+        exact
+        path="/admin"
+        render={() =>
+          this.props.loggedIn ? (
+            <VideoList videoData={this.props.videoData} />
+          ) : (
+            <Redirect to="/login" />
+          )
+        }
+      />
+      <Route exact path="/admin/videos" component={VideosAll} />
+      <Route
+        path="/admin/videos/:video_id"
+        render={() => <VideoDetail videoData={this.props.videoData} />}
+      />
+    </Switch>
+  );
+};
 
 export default AdminRoute;
--- app/client/src/CreatorDashboard/VideoOverview/VideoDetail.jsx
+++ app/client/src/CreatorDashboard/VideoOverview/VideoDetail.jsx
@@ -1,26 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-class VideoList extends React.Component {
-  constructor(props) {
-    super(props);
+const VideoDetail = props => {
+  const video = "";
+  // const video = videoIDs.get(props.match.params.video_id);
+
+  if (!video) {
+    return <div>Sorry, the video was not found</div>;
+  } else {
+    return (
+      <div>
+        <h1>Individual video page</h1>
+        <h2>{video.title}</h2>
+        <Link to="/videos">Back</Link>
+      </div>
+    );
   }
+};
 
-  render() {
-    const video = videoIDs.get(props.match.params.video_id);
-
-    if (!video) {
-      return (<div>Sorry, the video was not found</div>);
-    } else {
-      return (
-        <div>
-          <h1>Individual video page</h1>
-          <h2>{video.title}</h2>
-          <Link to="/videos">Back</Link>
-        </div>
-      );
-    }
-  }
-}
-
-export default Video;
+export default VideoDetail;
