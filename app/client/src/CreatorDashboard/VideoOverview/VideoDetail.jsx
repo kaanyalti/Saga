d06737c4a3f03fab71517f02c25d599cb1735616
Corrina Chow <corrina.yy.chow@gmail.com>
2018-06-06 18:18:03 +0000

    Import Reactions from getVideoDataMethods

--- app/client/src/CreatorDashboard/VideoOverview/VideoDetail.jsx
+++ app/client/src/CreatorDashboard/VideoOverview/VideoDetail.jsx
@@ -1,21 +1,38 @@
 import React from "react";
+import axios from 'axios'
 import { Link } from "react-router-dom";
+import * as Reactions from "../../modules/getVideoDataMethods"
 
-const VideoDetail = props => {
-  const video = "";
-  // const video = videoIDs.get(props.match.params.video_id);
 
-  if (!video) {
-    return <div>Sorry, the video was not found</div>;
-  } else {
-    return (
-      <div>
-        <h1>Individual video page</h1>
-        <h2>{video.title}</h2>
-        <Link to="/videos">Back</Link>
-      </div>
-    );
+// Reactions.retrieveVideoData(videoID)
+
+class VideoDetail extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+    };
   }
-};
+
+  componentDidMount() {
+
+  }
+
+  // const video = videoIDs.get(props.match.params.video_id);
+  render() {
+    const video = this.
+    if (!video) {
+      return <div>Sorry, the video was not found</div>;
+    } else {
+      return (
+        <div>
+          <h1>Individual video page</h1>
+          <h2>{video.title}</h2>
+          <Link to="/videos">Back</Link>
+        </div>
+      );
+    }
+  }
+}
 
 export default VideoDetail;
--- app/client/src/modules/getVideoDataMethods.js
+++ app/client/src/modules/getVideoDataMethods.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-export const retrieveKairos = () => {
+export const retrieveVideoData = (videoID) => {
   axios
-    .get("/api/kairos?kairos_method=retrieve")
-    .then(() => console.log("successfully connected to the server"))
+    .get(`/api/videos/${videoID}/reactions`)
+    .then(() => console.log("Successfully got video reactions data"))
     .catch(err => console.log("error: ", err));
 }
