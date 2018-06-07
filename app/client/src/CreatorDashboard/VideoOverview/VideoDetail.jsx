65d7c8a3c592ba533642a9b8d77cda5156ddf853
steveabouem <steveabouem@gmail.com>
2018-06-07 16:07:37 +0000

    started :videoId style, pulling from production before any further changes

--- app/client/src/CreatorDashboard/VideoOverview/VideoDetail.jsx
+++ app/client/src/CreatorDashboard/VideoOverview/VideoDetail.jsx
@@ -2,16 +2,11 @@ import React from "react";
 import videoIDs from "../../fakeYouTubeRes.js";
 import { Link } from "react-router-dom";
 import lottie from "lottie-web";
-// import {NotFoundEmoji} from "./NotFound";
 import NotFoundAnimation from "./NotFoundAnimation.jsx";
 
 class Video extends React.Component {
   constructor ( props ) {
     super ( props)
-    this.state = {
-      video: null
-      // videoIDs.get(props.match.params.video_id);
-    }
   }
 
    NotFoundStyle = {
@@ -31,27 +26,29 @@ class Video extends React.Component {
   }
   
   FoundOrNot = () => {
-    if (!this.state.video) {
+    if (videoIDs.videoIDs[0].id !== this.props.match.params.video_id) {
+      console.log("Params are different from youtube data");
         return <div style = {this.NotFoundStyle}>
         <div> <NotFoundAnimation/> </div>
-        <p style={this.PStyle}> Sorry, the video was not found. </p> 
+        <p style={this.PStyle} > Sorry, the video was not found. </p> 
         </div>;
       }
   }
-
-  
-  
-   
+  mapfunc = videoIDs.videoIDs.map( v => {
+    console.log(v.id);
+    return v.id
+  })
 
   render (){
-  return (
-    <div id="none">
-      <h1>Individual video page</h1>
-      {/* <h2>{video.title}</h2> */}
-      {this.FoundOrNot()}
-      <Link to="/videos">Back</Link>
-    </div>
-  );
+    this.mapfunc
+    return (
+      <div>
+        <h1>Individual video page</h1>
+        {/* <h2>{videoIDs}</h2> */}
+        {this.FoundOrNot()}
+        <Link to="/videos">Back</Link>
+      </div>
+    );
 };
 }
 export default Video;
