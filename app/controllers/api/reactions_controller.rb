299e2bcfdb0e47461033a7e8c34b61ecf6568386
Corrina Chow <corrina.yy.chow@gmail.com>
2018-06-07 06:23:28 +0000

    Filter empty nil data before sending

--- app/controllers/api/reactions_controller.rb
+++ app/controllers/api/reactions_controller.rb
@@ -2,11 +2,27 @@ class Api::ReactionsController < ApplicationController
 
   # GET /reactions
   def index
-    puts "INSIDE GET REQUEST"
-    @reactions = Video.find_by(youtube_id: params[:video_id]).reactions
+    @reactions = Video.find_by(youtube_id: params[:video_id]).reactions.to_a
 
-    render json: @reactions, status: :ok
+    @filtered = @reactions.map do |reaction|
+      filter(reaction[:apiData])
+    end.reject(&:blank?)
+
+    render json: @filtered, status: :ok
   end
+
+  private
+
+  def filter(res)
+    res.map do |entry|
+      entry["people"].map do |mood|
+        if mood["emotions"].values.inject { |a, b| a + b } > 0
+          entry
+        end
+      end.reject(&:blank?)
+    end.reject(&:blank?)
+  end
+
 end
 # create var here if this is the endpoont youre doing jur ajax on. try to aggregate data on the backed instead
 # of doing the whole wokr on th front end
