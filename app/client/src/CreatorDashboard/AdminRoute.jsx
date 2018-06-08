7b05ef2dfe5d4efabb2e853fc2db9a8b2bfc1e55
Corrina Chow <corrina.yy.chow@gmail.com>
2018-06-08 20:10:18 +0000

    Fix props being passed to VideoDetail

--- app/client/src/CreatorDashboard/AdminRoute.jsx
+++ app/client/src/CreatorDashboard/AdminRoute.jsx
@@ -16,13 +16,13 @@ class AdminRoute extends React.Component {
   render() {
     return (
       <div>
-      <Sidebar />
-        <Switch> 
+        <Sidebar />
+        <Switch>
           <Route
             exact
             path="/admin"
             render={() =>
-              this.props.loggedIn? (
+              this.props.loggedIn ? (
                 <VideoList videoData={this.props.videoData} />
               ) : (
                 <Redirect to="/login" />
@@ -30,7 +30,11 @@ class AdminRoute extends React.Component {
             }
           />
           <Route exact path="/admin/videos" component={VideosAll} />
-          <Route path="/admin/videos/:video_id" component={VideoDetail} />
+          <Route
+            path="/admin/videos/:video_id"
+            // Pass props to receive :video_id prop
+            render={(props) => <VideoDetail videoData={this.props.videoData} {...props} />}
+          />
         </Switch>
       </div>
     );
