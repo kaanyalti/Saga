ebb33c4831b872038af0933ec39ed5980a3deeb5
corrinachow <corrina.yy.chow@gmail.com>
2018-06-10 13:04:24 -0400

    Bypassing loggedIn conditional

--- app/client/src/App.jsx
+++ app/client/src/App.jsx
@@ -7,10 +7,17 @@ class App extends Component {
     super(props);
 
     this.state = {
-      loggedIn: false,
+      loggedIn: true,
       userEmail: null,
       firstName: null,
-      videoData: null
+      videoData: [
+    { id: "QJO3ROT-A4E", title: "What Makes You Beautiful" },
+    { id: "Jwgf3wmiA04", title: "Drag Me Down" },
+    { id: "W-TE_Ys4iwM", title: "Story of My Life" },
+    { id: "o_v9MY_FMcw", title: "Best Song Ever" },
+    { id: "AbPED9bisSc", title: "Live While We're Young" },
+    { id: "Y1xs_xPb46M", title: "One Thing" }
+  ]
     };
     this.handleLogin = this.handleLogin.bind(this);
   }
