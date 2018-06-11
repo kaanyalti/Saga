c110e0e573e895c2e9b0a12820093c450fa7766d
corrinachow <corrina.yy.chow@gmail.com>
2018-06-11 03:37:57 -0400

    Style cards, use image instead of embedded video

--- app/client/src/CreatorDashboard/VideoOverview/VideoList.jsx
+++ app/client/src/CreatorDashboard/VideoOverview/VideoList.jsx
@@ -7,12 +7,14 @@ import {
   CardBody,
   CardTitle,
   CardSubtitle,
+  CardHeader,
+  CardFooter,
+  Button,
   Container,
   Row,
   Col
 } from "reactstrap";
 import Moment from "react-moment";
-
 
 class VideoList extends React.Component {
   constructor(props) {
@@ -27,6 +29,20 @@ class VideoList extends React.Component {
     marginTop: "5vh"
   };
 
+  cardStyle = {
+    marginBottom: "5vh",
+  }
+
+  iframeStyle = {
+
+  }
+
+  spreadEvenly = {
+    display: "flex",
+    alignItems: "baseline",
+    justifyContent: "space-between"
+  };
+
   render() {
     return this.props.videoData.length === 0 ? (
       <p>Loading...</p>
@@ -34,31 +50,47 @@ class VideoList extends React.Component {
       <Container style={this.containerStyle}>
         <Row>
           {this.props.videoData.map(video => {
-            const { id, publishedAt, title } = video;
+            const { id, publishedAt, title, statistics, thumbnail } = video;
             return (
               <Col md={{ size: 4 }} key={id}>
-                <Card>
-                  <iframe
-                    title={title}
-                    width="100%"
-                    height="100%"
-                    src={`https://www.youtube.com/embed/${id}`}
-                    frameBorder="0"
-                    allow="autoplay; encrypted-media"
-                    allowFullScreen
-                  />
+                <Card style={this.cardStyle}>
+                  <CardHeader>{title}</CardHeader>
+                  <CardImg top width="100%" src={thumbnail.url} alt="Card image cap" />
                   <CardBody>
-                    <CardTitle>{title}</CardTitle>
-                    <CardSubtitle>
-                    <i class="fas fa-align-center"></i>
-                      Posted{" "}
-                      <Moment fromNow>
-                        {publishedAt.substring(0, publishedAt.length - 1)}
-                      </Moment>
-                    </CardSubtitle>
-                    <CardText></CardText>
-                    <Link to={`/admin/videos/${video.id}`}>{video.title}</Link>
+                    <CardSubtitle />
+                    <CardText>
+                      <div style={this.spreadEvenly}>
+                        <div className="stats">
+                          <i title="Amount of views" class="fas fa-eye" />{" "}
+                          <span>{statistics.viewCount}</span>
+                        </div>
+                        <div className="stats">
+                          <i title="Amount of likes" className="fas fa-heart" /> <span>{statistics.likeCount}</span>
+                        </div>
+                        <div className="stats">
+                          <i title="Amount of comments" class="fas fa-comments" />{" "}
+                          <span>{statistics.commentCount}</span>
+                        </div>
+                        <div className="stats">
+                          <i title="Amount of dislikes" class="fas fa-thumbs-down" />{" "}
+                          <span>{statistics.dislikeCount}</span>
+                        </div>
+                      </div>
+                    </CardText>
                   </CardBody>
+                  <CardFooter className="text-muted" style={this.spreadEvenly}>
+                    <div>
+                      <i title="Time since published" class="far fa-clock" />{" "}
+                      <span><Moment fromNow>
+                        {publishedAt.substring(0, publishedAt.length - 1)}
+                      </Moment></span>
+                    </div>
+                    <Button color="basic">
+                      <Link to={`/admin/videos/${video.id}`}>
+                        <i title="View data visualisations" className="fas fa-ellipsis-h" style={{color: "black"}}/>
+                      </Link>
+                    </Button>
+                  </CardFooter>
                 </Card>
               </Col>
             );
